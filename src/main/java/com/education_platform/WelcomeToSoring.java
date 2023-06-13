5fecae6593e4683e34830fa394e5b2cddf95e9d1
Ivanka-karaim <karaimivanna@gmail.com>
2023-06-13 18:58:51 +0300

    Initial commit

---
+++ src/main/java/com/education_platform/WelcomeToSoring.java
@@ -0,0 +1,13 @@
+package com.education_platform;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeToSoring {
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
---
+++ src/test/java/com/education_platform/EducationPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
